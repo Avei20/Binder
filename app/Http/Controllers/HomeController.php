61b15f40df6fd1fc0eec04b1c51b5ede0bcdcc7e
Avei20 <71458604+Avei20@users.noreply.github.com>
2022-01-18 08:21:58 +0700

    Filtering Match List Result

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -16,7 +16,23 @@ class HomeController extends Controller
 
     public function index()
     {
+        $pref_gender = Auth::user()->detail->gender;
+        if ($pref_gender == 'M') {
+            $pref_gender = 'F';
+        }
+        else if ($pref_gender == 'F') {
+            $pref_gender = 'M';
+        }
+
         $users = User::where('nim', '!=', Auth::id())->with('detail')->get();
+
+        $len = count($users);
+        for ($i=0; $i<$len; $i++) {
+            if ($users[$i]['detail']['gender'] != $pref_gender) {
+                unset($users[$i]);
+            }
+        }
+
         $currentuser = User::where('nim', '=', Auth::id())->first();
         $requestedmatch = MatchedList::where('nimUser', '=', Auth::id())->get();
         return view('pages.home', compact('users', 'currentuser', 'requestedmatch'));
